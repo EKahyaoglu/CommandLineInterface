1529c78b0ad6569aebcab4d70662fb09e1cc3124
Eren Kahyaoglu <efk40@scarletmail.rutgers.edu>
2025-01-05 09:36:21 -0500

    Added type builtin

--- app/main.py
+++ app/main.py
@@ -1,26 +1,34 @@
-#
-#
-#
+# Command Line Interface Program
+# Built with Python 3.11
 
 import sys
 
 def main():   
     
-    valid_commands = ["echo", "exit 0"]
+    valid_commands = ["echo", "exit 0", "type"]
     
-    # ----- EXPECT USER INPUT -----
+    # ----- GETTING USER INPUT -----
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
         command = input().strip()
-
-        if command.startswith("echo"):
+        
+        if command.startswith("type"):
+            type_cmd = command[len("type "):].strip()
+            if type_cmd.startswith("echo"):
+                print("echo is a shell builtin")
+            elif type_cmd.startswith("exit"):
+                print("exit is a shell builtin")
+            else:
+                print(f"type {type_cmd}: not found")
+            continue
+        elif command.startswith("echo"):
             echo_parts = command.split()
             echo_output = " ".join(echo_parts[1:])
             print(echo_output)    
         elif command == "exit 0":
             break
-        elif command not in valid_commands:
+        else:
             print(f"{command}: command not found")
             continue
         
