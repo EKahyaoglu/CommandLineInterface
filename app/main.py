e4007638af8cedd3fc0dd5273ba0a87239e2c526
Eren Kahyaoglu <efk40@scarletmail.rutgers.edu>
2025-01-05 09:19:49 -0500

    Added echo builtin

--- app/main.py
+++ app/main.py
@@ -1,32 +1,28 @@
+#
+#
+#
+
 import sys
 
 def main():   
     
-    valid_commands = []
+    valid_commands = ["echo", "exit 0"]
     
-    sys.stdout.write("$ ")
-    sys.stdout.flush()
-
-    # Wait for user input
-    command = input()
-    match command:
-        case "hello":
-            print("Hello, World!")
-        case "exit 0":
-            sys.exit()
-        case _:
-            print(f"{command}: command not found")
-    main() # Call recursively
-
-    """ ALTERNATIVE REPL IMPLEMENTATION
+    # ----- EXPECT USER INPUT -----
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
-        command = input()
-        if command not in valid_commands:
+        command = input().strip()
+
+        if command.startswith("echo"):
+            echo_parts = command.split()
+            echo_output = " ".join(echo_parts[1:])
+            print(echo_output)    
+        elif command == "exit 0":
+            break
+        elif command not in valid_commands:
             print(f"{command}: command not found")
             continue
-    """
-
+        
 if __name__ == "__main__":
     main()
