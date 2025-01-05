adf28f5b9fae94ea0afba8e3666f9118ff9a7088
Eren Kahyaoglu <efk40@scarletmail.rutgers.edu>
2025-01-04 23:14:15 -0500

    Implemented REPL

--- app/main.py
+++ app/main.py
@@ -1,13 +1,30 @@
 import sys
 
-
 def main():   
+    
+    valid_commands = []
+    
     sys.stdout.write("$ ")
+    sys.stdout.flush()
 
     # Wait for user input
     command = input()
-    print(f"{command}: command not found")
+    match command:
+        case "hello":
+            print("Hello, World!")
+        case _:
+            print(f"{command}: command not found")
+    main() # Call recursively
 
+    """
+    while True:
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
+        command = input()
+        if command not in valid_commands:
+            print(f"{command}: command not found")
+            continue
+    """
 
 if __name__ == "__main__":
     main()
