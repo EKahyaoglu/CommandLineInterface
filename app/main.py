b86e9ca09edd43dd35f02f8da81f1325591d376f
Eren Kahyaoglu <efk40@scarletmail.rutgers.edu>
2025-01-07 22:38:51 -0500

    improved type builtin

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 # Command Line Interface Program
 # Built with Python 3.11
 
-import sys
+import sys, os
 
 def main():   
-    
+    # ----- VALID COMMANDS -----
     valid_commands = ["echo", "exit 0", "type"]
     
     # ----- GETTING USER INPUT -----
@@ -13,17 +13,24 @@ def main():
         sys.stdout.flush()
         command = input().strip()
         
+        # Implementing type builtin command
         if command.startswith("type"):
             type_cmd = command[len("type "):].strip()
-            if type_cmd.startswith("echo"):
-                print("echo is a shell builtin")
-            elif type_cmd.startswith("exit"):
-                print("exit is a shell builtin")
-            elif type_cmd.startswith("type"):
-                print("type is a shell builtin")
+            if type_cmd in valid_commands:
+                print(f"{type_cmd} is a shell builtin")
             else:
-                print(f"{type_cmd}: not found")
+                found = False
+                for path_dir in os.environ.get("PATH", "").split(":"):
+                    potential_path = os.path.join(path_dir, type_cmd)
+                    if os.path.isfile(potential_path) and os.access(potential_path, os.X_OK):
+                        print(f"{type_cmd} is {potential_path}")
+                        found = True
+                        break
+                if not found:
+                    print(f"{type_cmd}: not found")
             continue
+
+        # Implementing echo and exit 0 builtin commands
         elif command.startswith("echo"):
             echo_parts = command.split()
             echo_output = " ".join(echo_parts[1:])
