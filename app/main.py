8201161730c5fe6b418c35669101b712e14dfabc
Eren Kahyaoglu <efk40@scarletmail.rutgers.edu>
2025-01-12 13:34:02 -0500

    Fixed issue with running external commands

--- app/main.py
+++ app/main.py
@@ -60,12 +60,14 @@ def run_command(command):
                 [executable_path] + args, check=True, capture_output=True, text=True
             )
 
+            program_signature = res.stdout.strip()
+
             # Print the expected output
             print(f"Program was passed {len(command_parts)} args (including program name).")
             print(f"Arg #0 (program name): {program_name}")
             for i, arg in enumerate(args):
                 print(f"Arg #{i+1}: {arg}")
-            print(f"Program Signature: {res.stdout.strip()}")
+            print(f"Program Signature: {program_signature}")
         except subprocess.CalledProcessError as e:
             print(e.stderr.strip())
     else:
