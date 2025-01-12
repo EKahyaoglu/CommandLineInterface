6bb8f87626cca0c88d97bdbb9f874617b8b05b19
Eren Kahyaoglu <efk40@scarletmail.rutgers.edu>
2025-01-12 11:57:03 -0500

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
-# Command Line Interface Program
-# Built with Python 3.11
+# POSIX Compliant Shell / Command Line Interface Program
 
 import sys, os, subprocess
 
-# ----- VALID COMMANDS -----
-def is_builtin(command):
-    return command in ["echo", "exit 0", "exit", "type"]
 
+# Check if command is a shell builtin
+def is_builtin(command):
+    return command in ["echo", "exit 0", "exit", "type", "pwd"]
+
+
+# Find the executable in the PATH environment variable
 def find_executable(command):
     for path_dir in os.environ.get("PATH", "").split(":"):
         potential_path = os.path.join(path_dir, command)
@@ -14,8 +16,11 @@ def find_executable(command):
             return potential_path
     return None
 
+
+# ----- COMMAND DIRECTORY -----
+# "type" Command
 def handle_type(command):
-    type_cmd = command[len("type "):].strip()
+    type_cmd = command[len("type ") :].strip()
     if is_builtin(type_cmd):
         print(f"{type_cmd} is a shell builtin")
     else:
@@ -25,11 +30,20 @@ def handle_type(command):
         else:
             print(f"{type_cmd}: not found")
 
+
+# "echo" Command
 def handle_echo(command):
     echo_parts = command.split()
     echo_output = " ".join(echo_parts[1:])
     print(echo_output)
 
+
+# "pwd" Command
+def handle_pwd(command):
+    print(os.getcwd())
+
+
+# Runs external commands with arguments
 def run_command(command):
     command_parts = command.split()
     program = command_parts[0]
@@ -38,28 +52,35 @@ def run_command(command):
     executable_path = find_executable(program)
     if executable_path:
         try:
-            res = subprocess.run([executable_path] + args, check=True, capture_output=True, text=True)
+            res = subprocess.run(
+                [executable_path] + args, check=True, capture_output=True, text=True
+            )
             print(res.stdout.strip())
         except subprocess.CalledProcessError as e:
             print(e.stderr.strip())
     else:
         print(f"{command}: command not found")
 
-def main():   
-    # ----- GETTING USER INPUT -----
+
+# ----- GET USER INPUT & EXECUTE COMMANDS -----
+def main():
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
         command = input().strip()
-        
-        if command.startswith("type"):
-            handle_type(command)
-        elif command.startswith("echo"):
-            handle_echo(command)
-        elif command == "exit 0":
-            break
+
+        if is_builtin(command.split()[0]):
+            if command.startswith("type"):
+                handle_type(command)
+            elif command.startswith("echo"):
+                handle_echo(command)
+            elif command.startswith("pwd"):
+                handle_pwd(command)
+            elif command == "exit 0":
+                break
         else:
             run_command(command)
+
 
 if __name__ == "__main__":
     main()
